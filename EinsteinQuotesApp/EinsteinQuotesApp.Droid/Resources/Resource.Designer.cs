f9423ca92a2a90f5ec1eb426e273a919abb8eb90
Juan <juansero29@gmail.com>
2017-06-27 10:54:42 -0500

    - Updated NuGet Packages

--- EinsteinQuotesApp/EinsteinQuotesApp.Droid/Resources/Resource.Designer.cs
+++ EinsteinQuotesApp/EinsteinQuotesApp.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace EinsteinQuotesApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::EinsteinQuotesApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
